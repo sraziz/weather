ce8538c1d85b0014303863679b9df946cf16ce4f
SRM <sraziz@gmail.com>
2021-11-14 18:22:02 -0800

    secondary commit

--- lookup/views.py
+++ lookup/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
 
 def home(request):
-	return render(request,'home.html',{})
+	
+	import json
+	import requests
+
+	api_request = requests.get("http://api.openweathermap.org/data/2.5/weather?q=Delhi&units=metric&appid=cd4415a99345841edbb9040348e3f2d6")
+
+
+	try:
+		api = json.loads(api_request.content)
+
+	except Exception as e:
+		api = "Error"
+
+	return render(request,'home.html',{'api':api})
 
 
 def about(request):
